75bcda169967c790898ac94a3fd2842e8dd9e7cc
jxl922 <jxl922@gmail.com>
2019-01-05 18:50:54 +0800

    remove unused files

--- CoreHooks/Models/EventModle.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreHooks.Controllers.Modles
-{
-    public class GitModel
-    {
-        public string Id { get; set; }
-        public string EventName { get; set; }
-        public string Content { get; set; }
-    }
-}
